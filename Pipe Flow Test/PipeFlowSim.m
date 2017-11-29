4eb063b9db5892ed19838d2ee1d84a88751063f3
eric-reeder <33557163+eric-reeder@users.noreply.github.com>
2017-11-29 00:14:50 -0500

    Pipe flow sim and grid class
    Started script for simulating flow in a pipe. Created Grid class for storing grid info for staggered grid.

---
+++ Pipe Flow Test/PipeFlowSim.m
@@ -0,0 +1,20 @@
+%% Pipe Flow Simulation
+%  Eric Reeder & Adam Zeloof
+
+%% Clean Up
+
+clear all
+close all
+clc
+
+%% Initialize grid
+
+xDim = 0.04;
+yDim = 0.1;
+dx = 0.002;
+dy = 0.002;
+
+duration = 1;
+dt = 0.0002;
+
+grid = Grid(xDim, dx, yDim, dy, duration, dt);
