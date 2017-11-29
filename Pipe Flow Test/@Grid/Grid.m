9c34767f578ae6c4b6d6aadd2c7b4565fc55e7a7
eric-reeder <33557163+eric-reeder@users.noreply.github.com>
2017-11-29 09:35:32 -0500

    Added setBoundaryConditions function

--- Pipe Flow Test/@Grid/Grid.m
+++ Pipe Flow Test/@Grid/Grid.m
@@ -70,6 +70,13 @@ classdef Grid < handle
         
         % Set initial conditions
         setInitialConditions(obj, u0, v0, P0)
+%         function setInitialConditions(obj, u0, v0, P0)
+% 
+%             obj.u(:,:,1) = u0;
+%             obj.v(:,:,1) = v0;
+%             obj.P(:,:,1) = P0;
+% 
+%         end
         
     end
     
--- Pipe Flow Test/PipeFlowSim.m
+++ Pipe Flow Test/PipeFlowSim.m
@@ -14,7 +14,28 @@ yDim = 0.1;
 dx = 0.002;
 dy = 0.002;
 
-duration = 1;
+duration = 0.01;
 dt = 0.0002;
 
-grid = Grid(xDim, dx, yDim, dy, duration, dt);
+pipeFlowGrid = Grid(xDim, dx, yDim, dy, duration, dt);
+
+%% Set initial conditions
+
+x = 0:dx:xDim;
+y = 0:dy:yDim;
+
+inletStart = 0.01;
+inletStartIndex = find(x == inletStart);
+inletEnd = 0.03;
+inletEndIndex = find(x == inletEnd);
+
+u0 = zeros(length(x)+1, length(y));
+v0 = zeros(length(x), length(y)+1);
+v0(inletStartIndex:inletEndIndex,1) = 0.1;
+P0 = zeros(length(x)+1, length(y)+1);
+
+pipeFlowGrid.u(:,:,1)
+
+pipeFlowGrid.setInitialConditions(u0, v0, P0);
+
+pipeFlowGrid.u(:,:,1)
