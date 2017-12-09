711cb7ffa2263cdf53e32dce244e4ab510355358
eric-reeder <33557163+eric-reeder@users.noreply.github.com>
2017-12-09 16:28:37 -0500

    Made sum changes

--- Dashpot Simulation/DashpotSim.m
+++ Dashpot Simulation/DashpotSim.m
@@ -7,8 +7,10 @@ clear all
 close all
 clc
 
-animate = false;
-saveAnimation = false;
+saveFilename = 'trial_2.mat';
+
+% animate = false;
+% saveAnimation = false;
 
 %% Initialize grid
 
@@ -22,7 +24,7 @@ dt = 0.00002;
 
 nu = 0.00002;
 
-blockVelocity = -0.02; % m/s
+blockVelocity = -0.06; % m/s
 timeStepsPerMove = round((abs(blockVelocity) * dt / dy)^(-1));
 
 w = 1;
@@ -51,52 +53,21 @@ grid.setInitialConditions(u0, v0, P0);
 
 %% Solve at each time step
 
+tic
 textprogressbar('Running Simulation: ');
 for n = 2:length(grid.t)
     textprogressbar(n/length(grid.t)*100);
     grid.solveIntermediateVelocity(n, nu, blockVelocity);
-%     if n == 2
-%         acceptableNansUint = sum(sum(isnan(grid.uF)));
-%         acceptableNansVint = sum(sum(isnan(grid.vF)));
-%     end
-%      if sum(sum(isnan(grid.uF(:,:)))) > acceptableNansUint
-%         disp('NaN found in uF');
-%         disp(n);
-%         break;
-%     end
-%     if sum(sum(isnan(grid.vF(:,:)))) > acceptableNansVint
-%         disp('NaN found in vF');
-%         disp(n);
-%         break;
-%     end
     grid.solvePressure(n, w);
     grid.solveFinalVelocity(n, blockVelocity);
-%     if n == 2
-%         acceptableNansU = sum(sum(isnan(grid.u(:,:,n))));
-%         acceptableNansV = sum(sum(isnan(grid.v(:,:,n))));
-%     end
     if mod(n, timeStepsPerMove) == 0
         grid.moveBox(-1);
         grid.P(:,:,n) = solvePressureBoundary(grid, grid.P(:,:,n));
         [grid.u(:,:,n), grid.v(:,:,n)] = solveVelocityBoundary(grid, grid.u(:,:,n), grid.v(:,:,n), blockVelocity);
     end
-%     if sum(sum(isnan(grid.P(:,:,n)))) > 0
-%         disp('NaN found in P');
-%         disp(n);
-%         break;
-%     end
-%     if sum(sum(isnan(grid.u(:,:,n)))) > acceptableNansU
-%         disp('NaN found in u');
-%         disp(n);
-%         break;
-%     end
-%     if sum(sum(isnan(grid.v(:,:,n)))) > acceptableNansV
-%         disp('NaN found in v');
-%         disp(n);
-%         break;
-%     end
 end
 textprogressbar('Done!');
+toc
 
 %% Unify Velocity
 
@@ -111,40 +82,48 @@ velMag = sqrt(uUnified.^2+vUnified.^2);
 maxVel = max(max(max(velMag)));
 minVel = min(min(min(velMag)));
 
+%% Save results
+
+timeStepSkip = 5;
+pressureOutput = grid.P(:,:,1:timeStepSkip:end);
+uOutput = uUnified(:,:,1:timeStepSkip:end);
+vOutput = vUnified(:,:,1:timeStepSkip:end);
+save(saveFilename,'pressureOutput','uOutput','vOutput');
+
 %% Make an Animation
 
-if animate
-    textprogressbar('Generating Animation: ');
-    frameSkip = 5;
-    animationLength = round(length(grid.t)/frameSkip);
-    frames = struct('cdata', cell(1,animationLength), 'colormap', cell(1,animationLength));
-    f = figure('visible', 'off','Position',[0 0 273 685]);
-    hold on;
-    for n = 1:animationLength
-        ind = n*frameSkip;
-        textprogressbar(n/animationLength*100);
-        surf(sqrt(vUnified(:,:,ind)'.^2+uUnified(:,:,ind)'.^2));
-        shading interp;
-        colormap(jet)
-        view(2);
-        axis image;
-        colorbar;
-        caxis([minVel maxVel]);
-        title('Velocity Magnitude')
-        drawnow;
-        pause(.01);
-        frames(n) = getframe(gcf);
-        clf;
-    end
-    hold off;
-    textprogressbar('Done!');
-end
+% if animate
+%     textprogressbar('Generating Animation: ');
+%     frameSkip = 5;
+%     animationLength = round(length(grid.t)/frameSkip);
+%     frames = struct('cdata', cell(1,animationLength), 'colormap', cell(1,animationLength));
+%     f = figure('visible', 'off','Position',[0 0 273 685]);
+%     hold on;
+%     for n = 1:animationLength
+%         ind = n*frameSkip;
+%         textprogressbar(n/animationLength*100);
+%         surf(sqrt(vUnified(:,:,ind)'.^2+uUnified(:,:,ind)'.^2));
+%         shading interp;
+%         colormap(jet)
+%         view(2);
+%         axis image;
+%         colorbar;
+%         caxis([minVel maxVel]);
+%         title('Velocity Magnitude')
+%         drawnow;
+%         pause(.01);
+%         frames(n) = getframe(gcf);
+%         clf;
+%     end
+%     hold off;
+%     textprogressbar('Done!');
+% end
 
 %% Plot results
 
 clf;
 close all;
-timeIndex = n;
+timeIndex = find(grid.t == 0.1);
 
 figure(1);
 surf(grid.P(:,:,timeIndex)');
@@ -181,18 +160,18 @@ axis image;
 colorbar;
 title('Velocity Magnitude')
 
-if animate
-    figure(6);
-    movie(frames,2,20);
-end
+% if animate
+%     figure(6);
+%     movie(frames,2,20);
+% end
 
 %% Save Video
-if animate && saveAnimation
-    vid = VideoWriter('CFDashpot.avi');
-    open(vid);
-    for k = 1:length(frames)
-        frame = frames(k);
-        writeVideo(vid,frame);
-    end
-    close(vid);
-end
+% if animate && saveAnimation
+%     vid = VideoWriter('CFDashpot.avi');
+%     open(vid);
+%     for k = 1:length(frames)
+%         frame = frames(k);
+%         writeVideo(vid,frame);
+%     end
+%     close(vid);
+% end
